b707f0ad036799380030adecc837bf414aab4a5c
Kazi Karim <naayel2016@gmail.com>
2017-01-21 16:06:32 -0500

    add method for excesise #1

--- src/method/practice/MethodExercise.java
+++ src/method/practice/MethodExercise.java
@@ -1,5 +1,31 @@
 package method.practice;
 
 public class MethodExercise {
+	
+	public static void main(String[] args) {
+		
+		String text = "hi";
+		System.out.println(getName(text));
+	
+	}
+	
+	
+	/** 1 Write a method that take your name input and
+	 * return your name
+	 * 
+	 * parameter : String 
+	 * return 	: String 
+	 * serving bucket : Variable
+	 */
+	
+	
+	public static String getName(String text){
+		String massage = "";
+		massage = text;
+		
+		
+		
+		return massage;
+	}
 
 }
